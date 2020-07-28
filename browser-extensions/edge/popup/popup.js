59f459584e642d3971f4943d9c9635711455a5f5
Sean Sanchez <atsanche@microsoft.com>
2020-07-27 19:05:09 -0700

    Enable Update of Profanity Terms (#5)
    * enable update of profanity terms
    
    * updates

--- browser-extensions/edge/popup/popup.js
+++ browser-extensions/edge/popup/popup.js
@@ -1,37 +1,44 @@
+updateBadWordsIfNone();
+
 const sendMessageId = document.querySelector("#obsceneText");
 if (sendMessageId) {
-  sendMessageId.onclick = function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(
-        tabs[0].id,
-        {
-          tabId: tabs[0].id
-        },
-        function (response) {
-          console.log("message with url sent");
-          //window.close();
-        }
-      );
-      function guidGenerator() {
-        const S4 = function () {
-          return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-        };
-        return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-      }
-    });
-  };
+    sendMessageId.onclick = function () {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(
+                tabs[0].id,
+                {
+                    tabId: tabs[0].id
+                },
+                function (response) {
+                    console.log("message with url sent");
+                    //window.close();
+                }
+            );
+            function guidGenerator() {
+                const S4 = function () {
+                    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+                };
+                return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+            }
+        });
+    };
 }
 
 const reportButtonId = document.querySelector("#reportButton");
 if (reportButtonId) {
-  reportButtonId.onclick = function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.executeScript(tabs[0].id, {
-        code: "window.getSelection().toString();"
-      }, function (selection) {
-        alert("Request has been sent for Review");
-      });
-    });
-  }
+    reportButtonId.onclick = function () {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.executeScript(tabs[0].id, {
+                code: "window.getSelection().toString();"
+            }, function (selection) {
+                alert("Request has been sent for Review");
+            });
+        });
+    }
 }
 
+$(document).ready(function () {
+    $("#updateFilterButton").click(function () {
+        updateBadWords();
+    });
+});
